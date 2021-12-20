ce060382a1714e2815573aba056a043d656c549d
Brian Prost <brian.s.prost@gmail.com>
2021-12-19 19:56:03 -0500

    fixed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["'Work Sans'", ...defaultTheme.fontFamily.sans],
-
+      },
+      colors: {
+        "jacpRed": "#e63946",
+        "jacpWhite": "#f1faee",
+        "jacpBlue": {
+          light: "#a8dadc",
+          smoke: "#457b9d",
+          navy: "#1d3557",
+          darkNavy: "#101E33",
+        },
       },
     },
   },
   plugins: [],
-}
+};
