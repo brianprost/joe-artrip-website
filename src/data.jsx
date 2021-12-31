e9dcb33dc6f48b766472f84e23c0d4e3affb58b9
Brian Prost <brian.s.prost@gmail.com>
2021-12-31 13:13:30 -0500

    added video links to unlisted YouTube videos

--- src/data.jsx
+++ src/data.jsx
@@ -1,4 +1,5 @@
 export const projectPortfolio = [
+    // he doesn't want this video anymore
     // {
     //     title: "Don't Be Caught Dead Without a Charge",
     //     roles: "Anker Charger Submission",
@@ -11,28 +12,28 @@ export const projectPortfolio = [
         roles: "Editor, Colorist",
         description: "My crew traveled to Ovapa, West Virginia to interview beekeeper, Eric Grandon, as part of a project highlighting local agriculture. In the documentary short film, we learned of his role in revitalizing the trade in the region in addition to the hardships he faced in the process.",
         image: "./img/sugar-bottom-bees-thumbnail.png",
-        link: "https://youtube.com",
+        link: "https://www.youtube.com/watch?v=SBy_EA53vds",
     },
     {
         title: "Picturebox Media",
         roles: "Graphics Animator",
         description: "In seeking to challenge myself in motion graphics, I created a splash screen for Picturebox Media, an upcoming  project YouTube channel. Utilizing After Effects’ in- program 3D tools, I sought to appeal to an intro that portrayed both kinetic typography and motion which synced to an ambient sound backlog.",
         image: "./img/picturebox-media-thumbnail.png",
-        link: "https://youtube.com",
+        link: "https://www.youtube.com/watch?v=So9scVCrIqM",
     },
     {
         title: "Sex Ed Embargo",
         roles: "Writer, Editor, Voice Over",
         description: "Sex Ed Embargo is my take on short form video infographics. In the piece, I compiled information and fair use archival Sex Ed serials to convey my critique on the US’ “Abstinence Only” sex education practices.",
         image: "./img/sex-ed-embargo-thumbnail.png",
-        link: "https://youtube.com",
+        link: "https://www.youtube.com/watch?v=R61SR7fzZzM",
     },
     {
         title: "Night. Car. Noir.",
         roles: "Director, Writer, Editor",
         description: "Night. Car. Noir. is a student film I directed satirizing 50s noir film tropes. I completed the film on a shoestring budget with a small crew according to the principles of a single location.",
         image: "./img/night-car-noir-thumbnail.png",
-        link: "https://youtube.com",
+        link: "https://www.youtube.com/watch?v=VE07CYFGDkw",
     },
 ];
 
