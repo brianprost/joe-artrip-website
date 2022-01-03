9cab6d64c60898fff1de97bdd0aa3f1ea2040d8a
Brian Prost <brian.s.prost@gmail.com>
2022-01-03 16:27:05 -0500

    better copy and paste behavior from contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import { FaEnvelopeOpenText, FaLinkedinIn } from "react-icons/fa";
 
 export default function Contact() {
+  const linkedInUrl = "https://www.linkedin.com/in/josephartrip/";
+  const emailAddress = "joe.h.artrip@gmail.com";
+
+  // const [referenceElement, setReferenceElement] = useState(null);
+  // const [popperElement, setPopperElement] = useState(null);
+  // const [arrowElement, setArrowElement] = useState(null);
+  // const { styles, attributes } = usePopper(referenceElement, popperElement, {
+  //   modifiers: [{ name: "arrow", options: { element: arrowElement } }],
+  // });
+
+  // const copyTooltip = () => {
+  //   createPopper
+  // }
+
   return (
     <section id="contact" className="bg-vhs-pattern-2">
       <div className="relative flex items-center justify-center min-h-screen sm:items-center sm:pt-0">
@@ -19,18 +33,37 @@ export default function Contact() {
                 >
                   <FaLinkedinIn className="bg-neutral-700 rounded-lg border-2 text-5xl text-color-white p-2 hover:scale-110 hover:bg-neutral-600 hover:text-jacpBlue-joeChoice" />
                 </a>
-                <div className="ml-4 text-lg tracking-wide font-lexend w-40">
+
+                <div
+                  className="ml-4 text-lg tracking-wide font-lexend w-40 select-all"
+                  onClick={() => {
+                    navigator.clipboard.writeText(linkedInUrl);
+                  }}
+                >
                   linkedin.com/in/josephartrip
                 </div>
+                {/* <div
+                  className="relative w-auto p-2 min-w-max rounded-md shadow-md bg-neutral-500 left-48 text-xs"
+                >
+                  Click to copy
+                </div> */}
               </div>
 
               <div className="flex items-center mt-2 text-white bg-neutral-500 rounded-lg bg-opacity-60 w-full md:w-2/3">
                 <a href="mailto:joe.h.artrip@gmail.com">
                   <FaEnvelopeOpenText className="bg-neutral-700 rounded-lg border-2 text-5xl text-color-white p-2 hover:scale-110 hover:bg-neutral-600 hover:text-jacpBlue-joeChoice" />
                 </a>
-                <div className="ml-4 text-lg tracking-wide font-lexend w-40">
+                <div
+                  className="ml-4 w-40 text-lg tracking-wide font-lexend select-all"
+                  onClick={() => {
+                    navigator.clipboard.writeText(emailAddress);
+                  }}
+                >
                   joe.h.artrip@gmail.com
                 </div>
+                {/* <span className="relative w-auto p-2 min-w-max rounded-md shadow-md bg-neutral-500 left-48 text-xs">
+                    Copy to clipboard
+                  </span> */}
               </div>
             </div>
           </div>
